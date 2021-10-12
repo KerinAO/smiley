a035391ca11ce28bd7b2cd93c047a88ad990ce8c
KerinAO <90246117+KerinAO@users.noreply.github.com>
2021-10-12 13:11:03 +0200

    Add files via upload

---
+++ Smiley/src/smiley/Smiley.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package smiley;
+
+/**
+ *
+ * @author korisnik
+ */
+public class Smiley {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+System.out.println("  xxxx  ");
+System.out.println(" x    x");
+System.out.println("x x  x x");
+System.out.println("x      x");
+System.out.println("x x  x x");
+System.out.println("x  xx  x");
+System.out.println(" x    x ");
+System.out.println("  xxxx  ");
+    
+}
+
+}
